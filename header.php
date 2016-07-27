c7c7186757d39b5db2b3d1403180d6bd42aac871
thiifranca <thiagofranca12@gmail.com>
2016-07-27 00:20:06 -0300

    teste

---
+++ header.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta property="og:locale" content="pt_BR">
+        <meta property="og:url" content="http://www.espacoodontoemfoco.com.br">
+        <meta property="og:title" content="Espaço Odonto em Foco">
+        <meta property="og:site_name" content="Espaço Odonto em Foco">
+        <meta property="og:description" content="Espaço Odonto em Foco">
+        <meta property="og:image" content="">
+        <meta property="og:image:type" content="image/jpeg">
+        <meta property="og:type" content="website">
+
+        <link rel="shortcut icon" href="library/images/icons/favicon.png">
+        <title>Espaço Odonto em Foco</title>
+
+        <!-- Cdm's -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="library/css/styles.min.css">
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+            <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body class="appear-animate">
+        <div class="page-loader">
+            <div class="loader">Loading...</div>
+        </div>
+        <span id="topo"></span>
+
+        <?php //include 'modais.php'; ?>
+
+        <header id="header">
+            <div class="container-fluid">
+                <div class="row">
+                    <a href="index.php" class="caLogo">
+                        <h1 class="logo">Espaço Odonto em Foco</h1>
+                    </a>
+                    
+                    <button class="abreMenu">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </button>                 
+                </div>
+            </div>
+        </header>
+
+        <navbar class="menuSite">
+            <nav id="menu">
+                <ul>
+                    <li>
+                        <a href="#"></a>
+                    </li>
+                    <li>
+                        <a href="#"></a>
+                    </li>
+                    <li>
+                        <a href="#"></a>
+                    </li>
+                    <li>
+                        <a href="#"></a>
+                    </li>
+                    <li>
+                        <a href="#"></a>
+                    </li>
+                    <li>
+                        <a href="#"></a>
+                    </li>
+                </ul>
+            </nav>
+        </navbar>
